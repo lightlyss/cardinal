a3fa48251b14137c8289b3e6e56acf0b3dab061c
Chara <37601555+lightlyss@users.noreply.github.com>
2019-08-23 02:35:34 -0400

    deploy model selector

---
+++ docs/precache-manifest.776334014f067765941840a8b9386c1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8cbafe3795733c2d134b88ec5daf5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02d1d46ddde9add7fdce",
+    "url": "./static/css/main.14c9e57f.chunk.css"
+  },
+  {
+    "revision": "e7ae699509b1629c2aa2",
+    "url": "./static/js/2.cbf8462a.chunk.js"
+  },
+  {
+    "revision": "02d1d46ddde9add7fdce",
+    "url": "./static/js/main.f99ae72e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
