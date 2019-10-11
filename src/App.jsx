77f21d0e395e2ae4e7047fe586c6f8e99a27d1b9
lightlyss <37601555+lightlyss@users.noreply.github.com>
2019-10-11 17:41:01 -0400

    restructure model archive and tracking

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,58 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentModel: 'shizuku',
-      models: ['Epsilon2.1', 'haru01', 'haru02', 'shizuku', 'tsumiki']
+      currentFamily: 'default',
+      currentModel: {"id": "000", "label": "Shizuku", "prefix": "shizuku"},
+      index: null
     };
   }
 
-  initL2D(model, width = 300, height = 600) {
+  initL2D(family, model, width = 300, height = 600) {
+    const jsonPath = `./models/${family}/${model.id}/`
+      + `${model.prefix ? model.prefix + '.' : ''}model.json`;
     L2Dwidget.init({
-      model: {
-        jsonPath: `./models/${model}/${model}.model.json`
-      },
-      display: { width, height }
+      model: {jsonPath},
+      display: {width, height}
     });
   }
 
-  handleChange(ev) {
-    const model = ev.target.value;
-    if (model === this.state.currentModel) return;
-    this.setState({currentModel: model})
-    window.location.search = `?model=${model}`;
+  handleFamilyChange(ev) {
+    const family = ev.target.value;
+    if (family === this.state.currentFamily) return;
+    this.setState({currentFamily: family});
   }
 
-  componentDidMount() {
+  handleModelChange(ev) {
+    const model = this.state.index[this.state.currentFamily]
+      .find(m => m.label === ev.target.value);
+    if (model.id === this.state.currentModel.id) return;
+    this.setState({currentModel: model});
+    window.location.search = `?model=${this.state.currentFamily}-${model.id}`;
+  }
+
+  async componentWillMount() {
+    const res = await fetch('./json/index.json');
+    const index = res.ok ? await res.json() : null;
+    this.setState({index: index});
+
     const params = new URLSearchParams(window.location.search);
-    const model = params.get('model');
-    if (model) this.setState({currentModel: model});
-    this.initL2D(model || this.state.currentModel);
+    const modelTag = params.get('model');
+    if (modelTag) {
+      const args = modelTag.split('-');
+      if (args[0] && args[1]) {
+        const family = args[0], id = args[1];
+        const model = index[family].find(m => m.id === id);
+        this.setState({currentFamily: family, currentModel: model});
+        return this.initL2D(family, model);
+      }
+    }
+    return this.initL2D(this.state.currentFamily, this.state.currentModel);
   }
 
   render() {
+    if (!this.state.index || !this.state.index[this.state.currentFamily]) {
+      return <div><h1>Loading...</h1></div>;
+    }
     return (<div>
       <h1>Cardinal</h1>
       <p>Live2D x ReactJS!</p>
@@ -42,11 +65,29 @@ class App extends React.Component {
         <fieldset>
           <legend>Settings</legend>
           <p>
+            <label htmlFor="select">Family:</label>
+            <select
+              value={this.state.currentFamily}
+              className="full"
+              id="selectf"
+              onChange={e => this.handleFamilyChange(e)}>
+              {
+                Object.keys(this.state.index)
+                  .map(f => <option key={f}>{f}</option>)
+              }
+            </select>
+          </p>
+          <p>
             <label htmlFor="select">Model:</label>
-            <select value={this.state.currentModel} className="full" id="select" onChange={e => this.handleChange(e)}>
-              {this.state.models.map(
-                m => <option key={m}>{m}</option>
-              )}
+            <select
+              value={this.state.currentModel.label}
+              className="full"
+              id="selectm"
+              onChange={e => this.handleModelChange(e)}>
+              {
+                this.state.index[this.state.currentFamily]
+                  .map(m => <option key={m.id}>{m.label}</option>)
+              }
             </select>
           </p>
         </fieldset>
