565c51db70d5a500b4f0612de0a03c9bd1ac5452
lightlyss <37601555+lightlyss@users.noreply.github.com>
2019-10-13 18:46:13 -0400

    update banner and revise tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,7 @@ import Puppeteer from 'puppeteer';
 
 const URL = 'http://localhost:3000/';
 let browser = null, page = null;
+let fSelector = null, mSelector = null;
 
 const waitOn = async selector => {
   return page.waitForSelector(selector, {
@@ -21,20 +22,35 @@ beforeAll(async () => {
   });
 });
 
-test('selects Live2D models', async () => {
+afterAll(async () => {
+  return browser.close();
+});
+
+test('loads Shizuku by default', async () => {
   await page.goto(URL);
   await waitOn('#selectf');
-  const fSelector = await page.$('#selectf');
-  const mSelector = await page.$('#selectm');
-
+  fSelector = await page.$('#selectf');
+  mSelector = await page.$('#selectm');
   await waitOn('#l2d-default-000');
   expect(page.url()).toBe(URL);
+});
+
+test('selects a different family', async () => {
   await fSelector.select('Magia Record');
   await mSelector.select('Holy Alina (EX)');
   await waitOn('#l2d-magireco-120800');
   expect(page.url()).toBe(`${URL}?model=magireco-120800`);
 });
 
-afterAll(async () => {
-  return browser.close();
+test('selects a different model', async () => {
+  await mSelector.select('Kagami Masara (Casual)');
+  await waitOn('#l2d-magireco-302902');
+  expect(page.url()).toBe(`${URL}?model=magireco-302902`);
+});
+
+test('selects the default family', async () => {
+  await fSelector.select('Default');
+  await mSelector.select('Haru (2)');
+  await waitOn('#l2d-default-002');
+  expect(page.url()).toBe(`${URL}?model=default-002`);
 });
